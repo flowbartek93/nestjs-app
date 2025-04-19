e63e6ea8ab77a014284fa666d937250c8c5ce970
Bartek Ostrowski <b.ostrowsk93@hotmail.com>
2025-04-19 19:40:29 +0200

    post i walidacja

--- src/tasks/create-task.dto.ts
+++ src/tasks/create-task.dto.ts
@@ -1,8 +1,26 @@
 import { TaskStatus } from './task.model';
 
+
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateTaskDto {
+  
+  @IsNotEmpty()
+  @IsString()
   id?: string;
+
+  
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  
+  @IsNotEmpty()
+  @IsString()
   description: string;
+
+
+  @IsEnum(TaskStatus)
   status: TaskStatus;
+
 }
