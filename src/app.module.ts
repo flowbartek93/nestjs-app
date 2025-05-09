1ed1430f05ccc3a659486307c7fbc63ea28d796a
bartlomiej.ostrowski <bartlomiej.ostrowski@media-press.tv>
2025-05-09 07:54:24 +0200

    config types

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,13 @@ import { TasksModule } from './tasks/tasks.module';
 import { ConfigModule } from '@nestjs/config';
 import { appConfig } from './config-files/app.config';
 import { appConfigSchema } from './config-files/config.types';
+import { typeOrmConfig } from './config-files/database.config';
+1;
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [appConfig],
+      load: [appConfig, typeOrmConfig],
       validationSchema: appConfigSchema,
       validationOptions: {
         aboutEarly: true,
--- src/config-files/config.types.ts
+++ src/config-files/config.types.ts
@@ -7,4 +7,9 @@ export interface ConfigType {
 
 export const appConfigSchema = Joi.object({
   APP_MESSAGEE_PREFIX: Joi.string().default('default prefix'),
+  DB_HOST: Joi.string().default('localhost'),
+  DB_PORT: Joi.number().default(5432),
+  DB_USERNAME: Joi.string().default('postgres'),
+  DB_PASSWORD: Joi.string().default('123'),
+  DB_DATABASE: Joi.string().default('tasks'),
 });
